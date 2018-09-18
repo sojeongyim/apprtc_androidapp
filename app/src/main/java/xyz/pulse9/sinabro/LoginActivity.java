fd82c62e6385cb0e238fa0f88e55dca70d098ff9
임소정 <pulse9.intern1@gmail.com>
2018-09-18 14:34:45 +0900

    follower

--- app/src/main/java/xyz/pulse9/sinabro/LoginActivity.java
+++ app/src/main/java/xyz/pulse9/sinabro/LoginActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.facebook.AccessToken;
--- app/src/main/java/xyz/pulse9/sinabro/TeacherlistFragment.java
+++ app/src/main/java/xyz/pulse9/sinabro/TeacherlistFragment.java
@@ -6,12 +6,15 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 
 /**
