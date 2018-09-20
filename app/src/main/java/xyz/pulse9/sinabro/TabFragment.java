182bcdfa46808c5e85cd928b03ae8a4943dceaba
임소정 <pulse9.intern1@gmail.com>
2018-09-20 17:16:33 +0900

    sojeong

--- app/src/main/java/xyz/pulse9/sinabro/TabFragment.java
+++ app/src/main/java/xyz/pulse9/sinabro/TabFragment.java
@@ -7,6 +7,7 @@ package xyz.pulse9.sinabro;
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -21,7 +22,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.firebase.FirebaseError;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
@@ -29,6 +32,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class TabFragment extends Fragment {
 
@@ -38,8 +42,10 @@ public class TabFragment extends Fragment {
 
         private String teacher1_token ="Jhbg1lLcwJRP7HcwHwVQwzJDy1H2";////intern1계정
         private DatabaseReference userDatabase;
+        DatabaseReference followerDB;
         private String uid;
         FirebaseUser curuser;
+        TextView followertext;
 
 
         @Override
@@ -49,7 +55,9 @@ public class TabFragment extends Fragment {
             curuser = FirebaseAuth.getInstance().getCurrentUser();
             uid = curuser.getUid();
             userDatabase = FirebaseDatabase.getInstance().getReference("users");
+            followerDB = userDatabase.child(teacher1_token).child("follower");
 
+            followertext =(TextView)getView().findViewById(R.id.follower_num);
             ImageView imageView_user1 = (ImageView) getView().findViewById(R.id.imageView_user1);
             ImageButton heart_butt = (ImageButton) getView().findViewById(R.id.user1_heart);
 
@@ -69,35 +77,23 @@ public class TabFragment extends Fragment {
 
                 @Override
                 public void onClick(View view) {
-//                    if(userDatabase.child(teacher1_token).child("follower").)
-                    userDatabase.child(teacher1_token).child("follower").push().child("id").setValue(uid);
+                    if(followerDB.child(uid).equals("1")==false) {
+                        followerDB.child(uid).setValue("1");
+                    }
 
                 }
             });
 
-            userDatabase.child(teacher1_token).addChildEventListener(new ChildEventListener() {
+            followerDB.addValueEventListener(new ValueEventListener() {
+
                 @Override
-                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                     Log.e(dataSnapshot.getKey(),dataSnapshot.getChildrenCount() + "");
+                    followertext.setText(Long.toString(dataSnapshot.getChildrenCount()));
                 }
 
                 @Override
-                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-                }
-
-                @Override
-                public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-                }
-
-                @Override
-                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-                }
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
+                public void onCancelled(@NonNull DatabaseError databaseError) {
 
                 }
             });
